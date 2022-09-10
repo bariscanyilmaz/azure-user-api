f9336b5e48702751b2397526a9510c0899f082bb
Barış Can Yılmaz <30300440+bariscanyilmaz@users.noreply.github.com>
2022-09-10 19:51:34 +0300

    create fakeuser api

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Bogus;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+var fakeUser=new Faker<User>();
+    fakeUser.RuleFor(x=>x.Id,f=>f.Random.Number(1_000_000))
+    .RuleFor(x=>x.Name,f=>f.Person.FirstName)
+    .RuleFor(x=>x.LastName,f=>f.Person.LastName)
+    .RuleFor(x=>x.Username,f=>f.Person.UserName)
+    .RuleFor(x=>x.Email,f=>f.Person.Email)
+    ;
+
+app.Map("/", () => fakeUser.Generate());
+
+app.Run();
